ae5577a053751bb7657cb8d907d827c56532e2ff
Davidebear <dbeardene@gmail.com>
2022-11-03 16:53:29 -0400

    demo2 final

--- demo2code/demo2code/demo2code.ino
+++ demo2code/demo2code/demo2code.ino
@@ -2,6 +2,9 @@
 // 0. IMPORT LIBRARIES
 //
 
+// OUR ASCII VALUE STARTS AT  85 (U)
+# define groupCharShift 84
+
 // Servos
 #include <Servo.h>
 Servo servoLeft;
@@ -9,13 +12,13 @@ Servo servoRight;
 
 // LCD
 #include <SoftwareSerial.h>
-#define TxPin 14
-SoftwareSerial mySerial = SoftwareSerial(255, TxPin);
+#define LCDPin 7
+SoftwareSerial mySerial = SoftwareSerial(255, LCDPin);
 
 // IR Sensor
 #include <Wire.h> // I2C library, required for MLX90614
-#include <SparkFunMLX90614.h> 
-IRTherm therm; 
+#include <SparkFunMLX90614.h>
+IRTherm therm;
 
 // Pins for QTI connections on board
 #define lineSensor1 49
@@ -29,7 +32,7 @@ const int pingPin = 35;
 # define Tx 16
 # define num 17
 
-// RGB 
+// RGB
 #define r 45
 #define g 46
 #define b 44
@@ -41,6 +44,18 @@ int totalHash = -1;
 // Store mission5 (cold) Hash
 int missionHash = -1;
 int doPrimary = -1;
+
+// Reading boolean
+int Reading = 0;
+
+// Arrays
+int group_scores[] = { -1, -1, -1, -1, -1};
+int printed[] = {0, 0, 0, 0, 0};
+int group_sum = 0;
+
+int getSum = 0;
+
+
 
 //
 // 1. FUNCTION DECLARATION
@@ -61,9 +76,15 @@ void hash_RGB(int, int);
 void mission5();
 
 void backupmission();
-float microsecondsToInches(long microseconds);
-float microsecondsToCentimeters(long microseconds);
+float microsecondsToInches(long);
+float microsecondsToCentimeters(long);
 float distance_in_inches();
+
+void showScore(int, int);
+void showSum(int);
+void showMod(int);
+
+void lightInd();
 
 
 //
@@ -71,10 +92,20 @@ float distance_in_inches();
 //
 void setup() {
 
-  // Initialize Serials (XBee and Arduino Serial)
+  // Initialize Serials (XBee and LCD)
+  pinMode(LCDPin, OUTPUT);
+  digitalWrite(LCDPin, HIGH);
   Serial.begin(9600); //start the serial monitor so we can view the output
-  Serial2.begin(9600); // Xbee Serial
+  delay(100);
+  mySerial.write(12); // clears LCD
+  delay(5);
+  mySerial.write(17);
+  digitalWrite(LCDPin, LOW);
+  delay(100);
+
+  Serial2.begin(9600);
   delay(500);
+
   Serial.println("Running diagnostics....\n");
   Serial.println("Serial monitor set up.");
 
@@ -105,19 +136,19 @@ void setup() {
   //
   if (doPrimary)
   {
-  pinMode(6, INPUT); // Uses pin 6 as the "switch"
-  totalHash = digitalRead(6) ? 6 : 5;  // Sets the total Hashes to 6 if wired.
-  Serial.println("Hashmark total set up.");
-  Serial.println(totalHash);
+    pinMode(6, INPUT); // Uses pin 6 as the "switch"
+    totalHash = digitalRead(6) ? 6 : 5;  // Sets the total Hashes to 6 if wired.
+    Serial.println("Hashmark total set up.");
+    Serial.println(totalHash);
 
-  // Joining I2C bus: for Thermo
-  Wire.begin(); 
-  Serial.println("IR Part 1 of 2 set up.");
+    // Joining I2C bus: for Thermo
+    Wire.begin();
+    Serial.println("IR Part 1 of 2 set up.");
 
-   // Initialize thermal IR
-  therm.begin();
-  Serial.println("IR Part 2/2 set up.");
-  therm.setUnit(TEMP_F); // Set the library's units to Farenheit (sic)
+    // Initialize thermal IR
+    therm.begin();
+    Serial.println("IR Part 2/2 set up.");
+    therm.setUnit(TEMP_F); // Set the library's units to Farenheit (sic)
   }
 
   // BACKUP MISSION
@@ -125,7 +156,7 @@ void setup() {
   //
   if (!doPrimary) {
     totalHash = 6;
-    
+
     pinMode(37, OUTPUT);
     pinMode(39, OUTPUT);
     digitalWrite(37, HIGH);
@@ -136,7 +167,7 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
 
-  }
+}
 
 void loop() {
   //
@@ -153,15 +184,15 @@ void loop() {
   int sRight = (qti3 < threshold) ? 1 : 0;
 
   int state = (4 * sLeft) + (2 * sMid) + (1 * sRight);
-//  Serial.println(state);
-//  Serial.println();
+  //  Serial.println(state);
+  //  Serial.println();
 
   //
   // Backup Mission Initialization
   //
-  
+
   float backup_array[6];
-  
+
 
   //
   // FUNCTION CALLS by STATE
@@ -172,23 +203,70 @@ void loop() {
       currentHash++;
       Serial.println("Current Hash:");
       Serial.println(currentHash);
-
-      
       halt(250);
 
+      // continuous loop that just focuses on reading the incoming data
+      while ((currentHash > (totalHash - 1)) && (Reading)) {
+
+        lightInd();
+        
+        if (Serial2.available()) {
+          char rawChar = Serial2.read();
+          delay(100);
+        }
+
+        
+        // expect a meaningful char to be 65 ASCII or greater
+        if (rawChar >= 65) {
+          lightInd();
+          int temp_group = getGroupNum(rawChar);
+          int temp_score = getGroupScore(rawChar);
+
+          group_scores[temp_group] = temp_score;
+
+          if ((printed[temp_group] == 0) && (temp_score != -1)) {
+            showScore(temp_group, temp_score);
+            printed[temp_group] = 1;
+          }
+        }
+
+        // check if sum can be calculated (assumes break works)
+
+        for (int i = 0; i <= 4; i++) {
+          if (group_scores[i] == -1) {
+            getSum = 0;
+            groupSum = 0;
+            break;
+          }
+          else {
+            groupSum += group_scores[i];
+            getSum = 1;
+          }
+        }
+
+        if (getSum) {
+          Reading = 0;
+          showSum(groupSum);
+          delay(100);
+        }
+
+
+
+      }
+
       //
-      // MISSION 5
+      // MISSION
       //
       if (doPrimary) {
-        if (currentHash <= (totalHash-1)) {
+        if (currentHash <= (totalHash - 1)) {
           mission5();
         }
         // Stop moving at Last Hash
-        check_stop(totalHash-1, currentHash);
+        check_stop(totalHash - 1, currentHash);
         // Keep moving if not Last Hash
         moveForward(200);
       }
-      
+
       else { // BACKUP
         // flash LED to show we doing stuff
         analogWrite(r, 255);
@@ -198,76 +276,78 @@ void loop() {
         analogWrite(r, 255);
         analogWrite(g, 255);
         analogWrite(b, 255);
-        delay(1500); 
-        
-        if (currentHash <= (totalHash-2)) {
+        delay(1500);
+
+        if (currentHash <= (totalHash - 2)) {
           backup_array[currentHash] = distance_in_inches();
           moveForward(200);
         }
-        else if (currentHash == (totalHash-1)) {
-            check_stop(1, 1); // Always True
-            
-            float smallest = backup_array[0];
-            int missionHash = 0;
+        else if (currentHash == (totalHash - 1)) {
+          check_stop(1, 1); // Always True
 
-            for (int i = 0; i < 6; i++) {
-              if (backup_array[i] < smallest) {
-                smallest = backup_array[i];
-                missionHash = i;
-              }
+          float smallest = backup_array[0];
+          int missionHash = 0;
+
+          for (int i = 0; i < 6; i++) {
+            if (backup_array[i] < smallest) {
+              smallest = backup_array[i];
+              missionHash = i;
             }
-        Serial2.print(missionHash + 1);
-        Serial.println("Found missionHash.");
-        Serial.println(missionHash + 1);
+          }
+
+          lightInd();
+          Serial2.print(missionHash + 1);
+          Serial.println("Found missionHash.");
+          Serial.println(missionHash + 1);
         }
         else {
           Serial.print("Backup done. missionHash found.");
         }
       }
-      
-      
+
+
       break;
 
-      
+
     case 1: // Coming on a left angle
       veerLeft(1);
       break;
-      
+
     case 2: // Highly unlikely
       Serial.println("What the (switch)???");
       break;
 
-      
+
     case 3:
       veerLeft(1);
       break;
 
-      
+
     case 4:
       veerRight(1);
       break;
 
-      
+
     case 5:
       moveForward(1);
       break;
 
-      
+
     case 6:
       veerRight(1);
       break;
 
-      
+
     case 7:
       Serial.println("Not on line");
       break;
 
-      
+
     default:
       halt(100);
       Serial.println("Uh oh. We not moving.");
       break;
-      
+
 
   }
 }
@@ -293,8 +373,8 @@ void veerLeft(int sec) {
 
 void veerRight(int sec) {
   // previous
-//  servoLeft.writeMicroseconds(1500 + 40);
-//  servoRight.writeMicroseconds(1500 + 25);
+  //  servoLeft.writeMicroseconds(1500 + 40);
+  //  servoRight.writeMicroseconds(1500 + 25);
   servoLeft.writeMicroseconds(1500 + 25);
   servoRight.writeMicroseconds(1500 - 10);
   delay(sec);
@@ -327,15 +407,18 @@ void tshootRight() {
 
 void check_stop(int totalHashes, int currentHash) {
   if (currentHash == totalHashes) {
-      servoLeft.detach();
-      servoRight.detach();
+    servoLeft.detach();
+    servoRight.detach();
   }
+
+  // set true to time to start Reading
+  Reading = 1;
 }
 
 void set_RGB(int red, int green, int blue) {
-  analogWrite(r, 255-red);
-  analogWrite(b, 255-blue);
-  analogWrite(g, 255-green);
+  analogWrite(r, 255 - red);
+  analogWrite(b, 255 - blue);
+  analogWrite(g, 255 - green);
 }
 
 void hash_RGB(int totalHashes, int currentHash) {
@@ -344,46 +427,46 @@ void hash_RGB(int totalHashes, int currentHash) {
       case 0:
         set_RGB(255, 0, 0); //red
         break;
-       case 1:
+      case 1:
         set_RGB(255, 255, 0); //yellow
         break;
-       case 2:
+      case 2:
         set_RGB(0, 255, 0); //green
         break;
-       case 3:
+      case 3:
         set_RGB(0, 0, 255); //blue
         break;
-       case 4:
+      case 4:
         set_RGB(0, 100, 100); //cyan
         break;
-       case 5:
+      case 5:
         set_RGB(102, 0, 204); // magenta
         break;
-       default:
+      default:
         set_RGB(0, 0, 0);
         break;
- 
+
     }
-    
+
   }
   else {
     switch (currentHash) {
-       case 0:
+      case 0:
         set_RGB(255, 0, 0); //red
         break;
-       case 1:
+      case 1:
         set_RGB(255, 255, 0); //yellow
         break;
-       case 2:
+      case 2:
         set_RGB(0, 255, 0); //green
         break;
-       case 3:
+      case 3:
         set_RGB(0, 0, 255); //blue
         break;
-       case 4:
+      case 4:
         set_RGB(0, 100, 100); //cyan
         break;
-       default:
+      default:
         set_RGB(0, 0, 0);
         break;
     }
@@ -391,50 +474,52 @@ void hash_RGB(int totalHashes, int currentHash) {
 }
 
 void mission5() {
-  
+
   digitalWrite(LED_BUILTIN, HIGH); // turn on light to show sensor doing stuff
   delay(500);
 
   // IR Sensor READS TEMPERATURE
-  if (therm.read()) 
+  if (therm.read())
   {
-    if (therm.object() < (therm.ambient() - 25)) 
+    if (therm.object() < (therm.ambient() - 25))
     {
-      
+
       set_RGB(0, 0, 255);
       delay(500);
-      
+
       missionHash = currentHash;
-      
-      set_RGB(0,0,0);
+
+      set_RGB(0, 0, 0);
       delay(1000);
-      
+
       set_RGB(255, 255, 255); // turn on to show XBee communication
 
       // Communicate to XBee
-      Serial2.print(missionHash+1);
-       
+      Serial2.print(missionHash + 1 + groupCharShift);
+
+
+
       delay(500);
-      set_RGB(0,0,0);
+      set_RGB(0, 0, 0);
     }
     else {
-      
-        set_RGB(255, 0, 0);
-        delay(500);
-        set_RGB(0,0,0);
+
+      set_RGB(255, 0, 0);
+      delay(500);
+      set_RGB(0, 0, 0);
     }
-    
+
   }
 
   digitalWrite(LED_BUILTIN, LOW);
-   
+
   //
   Serial.print("Object: " + String(therm.object(), 2));
   Serial.println("F");
   Serial.print("Ambient: " + String(therm.ambient(), 2));
   Serial.println("F");
   Serial.println();
-  }
+}
 
 
 float microsecondsToInches(long microseconds) {
@@ -449,38 +534,141 @@ float microsecondsToCentimeters(long microseconds) {
   return microseconds / 29.155 / 2.0;
 }
 
-float distance_in_inches(){
-      long duration;
-      float inches, cm;
-      // short LOW pulse to ensure a clean HIGH pulse:
-      pinMode(pingPin, OUTPUT);
-      digitalWrite(pingPin, LOW);
-      // 2 ms HIGH pulse
-      delayMicroseconds(2);
-      digitalWrite(pingPin, HIGH);
-      // back to LOW
-      delayMicroseconds(5);
-      digitalWrite(pingPin, LOW);
+float distance_in_inches() {
+  long duration;
+  float inches, cm;
+  // short LOW pulse to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  // 2 ms HIGH pulse
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  // back to LOW
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
 
-      // The same pin is used to read the signal from thPINGe ))): 
-      // a HIGH pulse whose duration is the time (in microseconds) 
-      // from the sending of the ping to the reception of 
-      // its echo off of an object.
-      pinMode(pingPin, INPUT);
-      duration = pulseIn(pingPin, HIGH);
+  // The same pin is used to read the signal from thPINGe ))):
+  // a HIGH pulse whose duration is the time (in microseconds)
+  // from the sending of the ping to the reception of
+  // its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
 
-      // convert the time into a distance
-      inches = microsecondsToInches(duration);
-      cm = microsecondsToCentimeters(duration);
-  
-      Serial.print(inches);
-      Serial.print("in, ");
-      Serial.print(cm);
-      Serial.print("cm");
-      Serial.println();
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
 
-      return inches;
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  return inches;
 }
 
+int getGroupNum(char rawChar) {
+  int temp = ((((rawChar - 65) - ((rawChar - 65) % 5)) / 5) + 1);
+  return temp;
+}
 
-  
+int getGroupScore(char rawChar) {
+  int temp = ((rawChar % 5) + 1);
+  return temp;
+}
+
+void showScore(int group, int score) {
+
+  switch (group) {
+
+    case 1:
+
+      mySerial.write(128);
+
+      mySerial.print("1=");
+
+      mySerial.print(score);
+
+      break;
+
+
+
+    case 2:
+
+      mySerial.write(132);
+
+      mySerial.print("2=");
+
+      mySerial.print(score);
+
+      break;
+
+
+
+    case 3:
+
+      mySerial.write(136);
+
+      mySerial.print("3=");
+
+      mySerial.print(score);
+
+      break;
+
+
+
+    case 4:
+
+      mySerial.write(140);
+
+      mySerial.print("4=");
+
+      mySerial.print(score);
+
+      break;
+
+
+
+    case 5:
+
+      mySerial.write(148);
+
+      mySerial.print("5=");
+
+      mySerial.print(score);
+
+      break;
+
+    default:
+
+      mySerial.println("Error in showScore");
+      break;
+
+  }
+
+
+  void showMod(int mod) {
+    mySerial.write(157);
+    mySerial.print("M=");
+    mySerial.print(mod);
+  }
+
+  void showSum(int sum) {
+    mySerial.write(152);
+    mySerial.print("S=");
+    mySerial.print(sum);
+  }
+
+  void lightInd() {
+
+    // flash onboard
+    analogWrite(r, 255);
+    analogWrite(g, 0);
+    analogWrite(b, 255);
+    delay(500);
+    analogWrite(r, 255);
+    analogWrite(g, 255);
+    analogWrite(b, 255);
+    delay(500);
+
+  }
